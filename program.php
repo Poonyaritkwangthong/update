5fc53b2675f55d26218205b8d03a66cb57a82414
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2023-01-06 12:28:08 +0700

    Add files via upload

---
+++ program.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="program1.css">
+</head>
+<body>
+<div class="nav">
+    <ul>
+      <li class="home"><a href="index.php">HOME</a></li>
+      <li class="tutorials"><a href="store.php">STROE</a>
+      <ul>
+        <li><a href="#">Tutorials #1</a></li>
+        <li><a href="#">Tutorials #2</a></li>
+        <li><a href="#">Tutorials #3</a></li>
+      </ul>
+    </li>
+    <li class="about"><a class="active" href="#">PROGRAM</a>
+      <ul>
+        <li><a href="program.php">News #1</a></li>
+        <li><a href="#">News #1</a></li>
+        <li><a href="#">News #1</a></li>
+      </ul>
+    </li>
+    <li class="News"><a href="#">REGISER</a></li>
+    <li href="Contact"><a href="login.php">LOGIN</a></li>
+    </ul>
+</div>
+<div class="head">
+    <h1>PROGRAM</h1>
+</div>
+<div class="main1">
+  <div class="flex">
+    <div class="img">
+        <img src="/6414421006/project/image/photo2.jpg">
+    </div>
+    <div class="content">
+      <div class="border">
+        <h1>FIRST STEP</h1>
+      </div>  
+         <h3>Sketching is very important in drawing.</h3>
+         <p>Let's start by specifying the proportions of the components.</p>
+    </div>
+  </div>
+      <div class="head2">
+        <h1>Sample video</h1>
+      </div>
+      <div class="video">
+      <iframe width="1088" height="612" src="https://www.youtube.com/embed/KakMVgP_q-w" title="วาดคนต้องรู้ "สัดส่วน" | Drawing body proportions" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+</div>
+</body>
+</html>
---
+++ store.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="store.css">
+</head>
+<body>
+    <div class="nav">
+        <ul>
+          <li class="home"><a href="index.php">HOME</a></li>
+          <li class="tutorials"><a href="store.php">STROE</a>
+          <ul>
+            <li><a href="#">Tutorials #1</a></li>
+            <li><a href="#">Tutorials #2</a></li>
+            <li><a href="#">Tutorials #3</a></li>
+          </ul>
+        </li>
+        <li class="about"><a class="active" href="#">PROGRAM</a>
+          <ul>
+            <li><a href="program.php">News #1</a></li>
+            <li><a href="#">News #1</a></li>
+            <li><a href="#">News #1</a></li>
+          </ul>
+        </li>
+        <li class="News"><a href="#">REGISER</a></li>
+        <li href="Contact"><a href="login.php">LOGIN</a></li>
+        </ul>
+      </div>
+      <div class="head">
+        <h1>
+            STROE
+        </h1>
+      </div>
+    <div class="maintop">
+      <div class="mainleft">
+        <div>
+            <img class="img1" herf="" src="">
+        </div>
+        <div class="content">
+            <div class="center">
+              <h1>name image</h1>
+              <p>price</p>
+              <p>type</p>
+              <p>painter</p>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="ft-l">
+                <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="">
+                <a>follow</a>
+            </div>
+            <div class="ft-r">
+                <a>like</a>
+                <img src="https://hotemoji.com/images/dl/j/growing-heart-emoji-by-twitter.png" alt="">
+            </div>
+        </div>
+      </div>
+      <div class="maincenter">
+        <div>
+            <img class="img1" herf="" src="">
+        </div>
+        <div class="content">
+            <div class="center">
+              <h1>name image</h1>
+              <p>price</p>
+              <p>type</p>
+              <p>painter</p>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="ft-l">
+                <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="">
+                <a>follow</a>
+            </div>
+            <div class="ft-r">
+                <a>like</a>
+                <img src="https://hotemoji.com/images/dl/j/growing-heart-emoji-by-twitter.png" alt="">
+            </div>
+        </div>
+      </div>
+      <div class="mainright">
+        <div>
+            <img class="img1" herf="" src="">
+        </div>
+        <div class="content">
+            <div class="center">
+              <h1>name image</h1>
+              <p>price</p>
+              <p>type</p>
+              <p>painter</p>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="ft-l">
+                <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="">
+                <a>follow</a>
+            </div>
+            <div class="ft-r">
+                <a>like</a>
+                <img src="https://hotemoji.com/images/dl/j/growing-heart-emoji-by-twitter.png" alt="">
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="mainlow">
+        <div class="mainleft">
+          <div>
+              <img class="img1" herf="" src="">
+          </div>
+          <div class="content">
+              <div class="center">
+                <h1>name image</h1>
+                <p>price</p>
+                <p>type</p>
+                <p>painter</p>
+              </div>
+          </div>
+          <div class="footer">
+              <div class="ft-l">
+                  <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="">
+                  <a>follow</a>
+              </div>
+              <div class="ft-r">
+                  <a>like</a>
+                  <img src="https://hotemoji.com/images/dl/j/growing-heart-emoji-by-twitter.png" alt="">
+              </div>
+          </div>
+        </div>
+        <div class="maincenter">
+          <div>
+              <img class="img1" herf="" src="">
+          </div>
+          <div class="content">
+              <div class="center">
+                <h1>name image</h1>
+                <p>price</p>
+                <p>type</p>
+                <p>painter</p>
+              </div>
+          </div>
+          <div class="footer">
+              <div class="ft-l">
+                  <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="">
+                  <a>follow</a>
+              </div>
+              <div class="ft-r">
+                  <a>like</a>
+                  <img src="https://hotemoji.com/images/dl/j/growing-heart-emoji-by-twitter.png" alt="">
+              </div>
+          </div>
+        </div>
+        <div class="mainright">
+          <div>
+              <img class="img1" herf="" src="">
+          </div>
+          <div class="content">
+              <div class="center">
+                <h1>name image</h1>
+                <p>price</p>
+                <p>type</p>
+                <p>painter</p>
+              </div>
+          </div>
+          <div class="footer">
+              <div class="ft-l">
+                  <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="">
+                  <a>follow</a>
+              </div>
+              <div class="ft-r">
+                  <a>like</a>
+                  <img src="https://hotemoji.com/images/dl/j/growing-heart-emoji-by-twitter.png" alt="">
+              </div>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
